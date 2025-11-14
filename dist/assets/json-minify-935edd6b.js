85d52d0b640010737cbd93ced22c6fef19a1ff27
pengxb <master@pengxb.com>
2025-11-14 16:35:58 +0800

    dist

--- dist/assets/json-minify-935edd6b.js
+++
@@ -1,5 +0,0 @@
-import{_ as a}from"./FormatTransformer.vue_vue_type_script_setup_true_lang-3c7bc3c3.js";import{l as r}from"./index-231c3e11.js";import{w as i}from"./defaults-4d6daddf.js";import{d as s,o as l,c as p}from"./index-ed0f64fe.js";import"./TextareaCopyable-a820648f.js";import"./Copy-ed7c7e97.js";import"./Scrollbar-16b7213b.js";const u=`{
-	"hello": [
-		"world"
-	]
-}`,N=s({__name:"json-minify",setup(m){const t=o=>i(()=>JSON.stringify(r.parse(o),null,0),""),e=[{validator:o=>o===""||r.parse(o),message:"Provided JSON is not valid."}];return(o,f)=>{const n=a;return l(),p(n,{"input-label":"Your raw JSON","input-default":u,"input-placeholder":"Paste your raw JSON here...","output-label":"Minified version of your JSON","output-language":"json","input-validation-rules":e,transformer:t})}}});export{N as default};
---
+++ dist/assets/json-minify-c10a6732.js
@@ -0,0 +1,5 @@
+import{_ as a}from"./FormatTransformer.vue_vue_type_script_setup_true_lang-3c7a1d9b.js";import{l as r}from"./index-231c3e11.js";import{w as i}from"./defaults-4d6daddf.js";import{d as s,o as l,c as p}from"./index-b4559a98.js";import"./TextareaCopyable-5ccce8e7.js";import"./Copy-53a6ea91.js";import"./Scrollbar-69041cd3.js";const u=`{
+	"hello": [
+		"world"
+	]
+}`,N=s({__name:"json-minify",setup(m){const t=o=>i(()=>JSON.stringify(r.parse(o),null,0),""),e=[{validator:o=>o===""||r.parse(o),message:"Provided JSON is not valid."}];return(o,f)=>{const n=a;return l(),p(n,{"input-label":"Your raw JSON","input-default":u,"input-placeholder":"Paste your raw JSON here...","output-label":"Minified version of your JSON","output-language":"json","input-validation-rules":e,transformer:t})}}});export{N as default};
